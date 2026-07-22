08d74830d75fe340c6fda53cffe5ef1b41caf33e
ajduncan670-rgb <ajduncan670@gmail.com>
2026-07-21 23:04:43 -0400

    Update auth-config.js

--- auth-config.js
+++ auth-config.js
@@ -5,7 +5,7 @@
 // ANON KEY is safe to expose in frontend code.
 // NEVER put your service_role key here.
 
-const SUPABASE_URL  = 'https://fcewnsodztrwyeeensof.supabase.co/rest/v1/';
+const SUPABASE_URL  = 'https://fcewnsodztrwyeeensof.supabase.co';
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjZXduc29kenRyd3llZWVuc29mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NzYzNjQsImV4cCI6MjEwMDI1MjM2NH0.Yi8FrYE0LmMFiia9rm6syL-QiM8P2wsWsx6sybKMppA';
 
 // ─── DO NOT EDIT BELOW THIS LINE ─────────────────────────────
